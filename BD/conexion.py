a74a910ba99161b0e486bbe2bbe8b0d4658cec1b
francisco casado <122122722+CasadoFrancisco@users.noreply.github.com>
2023-05-10 00:18:57 -0300

    change

--- BD/conexion.py
+++ BD/conexion.py
@@ -10,6 +10,7 @@ class Conexion():
                 password="2BhNwieRJdRe",
                 database="kevinahu_universidad"
             )
+            
         except Error as ex:
             print(f"Error al intentar la conexion: {0}".format(ex))
     
@@ -24,24 +25,4 @@ class Conexion():
                 print(f"Error al intentar la conexion: {0}".format(ex))
 
 
-#creando el alta del profesor a mysql
-def crearProfesor(self, nombre, apellido, asignatura):
-    if self.conexion.is_connected():
-        try:
-            cursor = self.conexion.cursor()
-            # Generamos un ID único para el profesor
-            cursor.execute("SELECT MAX(id_profesor) FROM profesor")
-            max_id = cursor.fetchone()[0]
-            if max_id is None:
-                id_profesor = 1
-            else:
-                id_profesor = max_id + 1
-            # Insertamos los datos del profesor en la tabla correspondiente
-            sql = "INSERT INTO profesor (id_profesor, nombre, apellido, asignatura) VALUES (%s, %s, %s, %s)"
-            val = (id_profesor, nombre, apellido, asignatura)
-            cursor.execute(sql, val)
-            self.conexion.commit()
-            print("Profesor creado exitosamente")
-        except Error as ex:
-            self.conexion.rollback()
-            print(f"Error al intentar crear el profesor: {0}".format(ex))
+    
--- principal.py
+++ principal.py
@@ -26,14 +26,23 @@ def menuPrincipal():
                 opcionCorrecta = True
                 ejecutarOpcion(opcion)
 
-    def ejecutarOpcion(opcion):
-        conexion = Conexion()
-        if opcion == 1:
-            print("============ ALTA DE PROFESOR ============")
-            nombre = input("Ingrese el nombre del profesor: ")
-            apellido = input("Ingrese el apellido del profesor: ")
-            asignatura = input("Ingrese la asignatura del profesor: ")
-            conexion.crearProfesor(nombre, apellido, asignatura)    
+def ejecutarOpcion(opcion):
+    conexion = Conexion()
+
+    if opcion == 1:
+        print("yaa")
+    elif opcion == 2:
+        print("alta asign")
+    elif opcion == 3:
+        print("alta alum")
+    elif opcion == 4:
+        print("ins de alumo a asig")
+    elif opcion == 5:
+        print("opcion 5")
+    elif opcion == 6:
+        print("opcion 6")
+    
+
   
    
 menuPrincipal()
